199c55c38521fd3b2a74da80c17e0bba409c94d8
timewizhan <timewizhan@gmail.com>
2015-09-18 17:55:21 +0900

    [AroundYouiPhone][timewizhan][RV:N/A] 1. iPhone 코드 생

---
+++ Mobile/iPhone/AroundYou/AroundYouTests/AroundYouTests.swift
@@ -0,0 +1,36 @@
+//
+//  AroundYouTests.swift
+//  AroundYouTests
+//
+//  Created by 물통꿀꿀이 on 2015. 9. 18..
+//  Copyright (c) 2015년 cclab. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class AroundYouTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
