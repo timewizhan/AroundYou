4301f607cb5ad2eaa2c2583f816f17b509accc9a
timewizhan <timewizhan@gmail.com>
2015-10-15 16:18:46 +0900

    [AroundYouiPhone][timewizhan][RV:N/A] 소스 트리 수정

--- Mobile/iPhone/AroundYou/AroundYou/StoresViewController.swift
+++ Mobile/iPhone/AroundYou/AroundYou/StoresViewController.swift
@@ -11,8 +11,6 @@ import UIKit
 class StoresViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     @IBOutlet weak var tableViewStores: UITableView!
 
-    @IBOutlet weak var menuButton: UIBarButtonItem!
-
     let textCellIdentifier = "cellStores"
     var arrayStores : [String] = ["부산식당", "형제갈비", "미가", "맥도날드", "버거킹"]
     
@@ -21,12 +19,6 @@ class StoresViewController: UIViewController, UITableViewDataSource, UITableView
         // Do any additional setup after loading the view, typically from a nib.
         tableViewStores.delegate = self
         tableViewStores.dataSource = self
-        
-        if self.revealViewController() != nil {
-            menuButton.target = self.revealViewController()
-            menuButton.action = "revealToggle:"
-            self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
-        }
     }
     
     override func didReceiveMemoryWarning() {
@@ -63,7 +55,6 @@ class StoresViewController: UIViewController, UITableViewDataSource, UITableView
     // MARK: - Navigation
     
     // In a storyboard-based application, you will often want to do a little preparation before navigation
-    
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
