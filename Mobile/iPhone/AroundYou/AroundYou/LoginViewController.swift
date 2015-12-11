277f4d88778389d1c51050a779659a0df43e4b71
timewizhan <timewizhan@gmail.com>
2015-12-11 21:52:55 +0900

    [AroundYouiPhone][timewizhan][RV:N/A] 1. 로그인 창 추가 (드래그를 통해 로그인) 2. 슬라이드바 이미지 추가

--- Mobile/iPhone/AroundYou/AroundYou/LoginViewController.swift
+++ Mobile/iPhone/AroundYou/AroundYou/LoginViewController.swift
@@ -10,15 +10,24 @@ import UIKit
 import CoreTelephony
 
 class LoginViewController: UIViewController {
-
+    @IBOutlet weak var imageForDrag: UIImageView!
+    @IBOutlet weak var imagebarForDrag: UIImageView!
+    
+    var imagePosition : CGPoint = CGPoint(x: 0, y: 0)
+    var imageLimitedMinX : CGFloat = 0
+    var imageLimitedMaxX : CGFloat = 0
+    var touchedImage : Bool = false
+    var loginToPermit : Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         //NSThread.sleepForTimeInterval(3)
         // Do any additional setup after loading the view.
-        if self.revealViewController() != nil {
-                print("dfdf")
-        }
+        
+        // To get data of image size
+        findCurrentImagePosition()
+        findImageBarPosition()
     }
 
     override func didReceiveMemoryWarning() {
@@ -26,9 +35,85 @@ class LoginViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    /*
-        To do..
-    */
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        if let touch : UITouch = touches.first! {
+            let positionCurrentTouch = touch.locationInView(self.view)
+            
+            let touchXPosition = positionCurrentTouch.x
+            let touchYPosition = positionCurrentTouch.y
+            
+            let rectImage : CGRect = imageForDrag.frame
+            let maxXPosition = rectImage.origin.x + rectImage.width
+            let minXPosition = rectImage.origin.x
+            let maxYPosition = rectImage.origin.y + rectImage.height
+            let minYPosition = rectImage.origin.y
+            
+            if touchXPosition > minXPosition && touchXPosition < maxXPosition {
+                if touchYPosition > minYPosition && touchYPosition < maxYPosition {
+                    // touch image for drag
+                    touchedImage = true
+                    debugPrint("Touch ImageView")
+                }
+            }
+        }
+    }
+    
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        if touchedImage != true {
+            return
+        }
+        
+        if let touch : UITouch = touches.first! {
+            let positionCurrentTouch = touch.locationInView(self.view)
+            
+            let touchXPosition = positionCurrentTouch.x
+            let rectImage : CGRect = imageForDrag.frame
+            
+            let positionToMove = touchXPosition + rectImage.width / 2
+            if positionToMove < imageLimitedMinX || positionToMove > imageLimitedMaxX {
+                return
+            }
+            
+            imagePosition.x = positionToMove
+            imageForDrag.center = imagePosition
+            
+            if imagePosition.x >= imageLimitedMaxX - 50 && imagePosition.x <= imageLimitedMaxX {
+                loginToPermit = true
+                debugPrint("Login to permit")
+                
+                moveToNextView()
+            }
+        }
+    }
+    
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        if touchedImage == true {
+            touchedImage = false
+            
+            // if user end touch, the image have to returned to first position
+            imagePosition.x = imageLimitedMinX
+            imageForDrag.center = imagePosition
+        }
+    }
+    
+    func findCurrentImagePosition() {
+        let rectImage : CGRect = imageForDrag.frame
+        let centerX = rectImage.width / 2 + rectImage.origin.x
+        let centerY = rectImage.height / 2 + rectImage.origin.y
+        
+        imagePosition = CGPoint(x: centerX, y: centerY)
+    }
+    
+    func findImageBarPosition() {
+        let rectImage : CGRect = imagebarForDrag.frame
+        imageLimitedMinX = rectImage.origin.x + imageForDrag.frame.width / 2
+        imageLimitedMaxX = rectImage.origin.x + rectImage.width
+    }
+
+    func moveToNextView() {
+        self.performSegueWithIdentifier("completedLogin", sender: nil)
+    }
     
     /*
     // MARK: - Navigation
